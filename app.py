81ba41905e7a432a8ee79c0e1dd4bd0944c9ec34
Denny Schaedig <dennys@wustl.edu>
2025-05-21 18:07:02 -0600

    Uploading development level app.py

--- app.py
+++ app.py
@@ -51,5 +51,8 @@ def tools():
     return render_template("tools.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
